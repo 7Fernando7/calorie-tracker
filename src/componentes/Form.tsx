9ae50338d6db061bb9f5c2b6d6dd3b79ea16b58d
Fernando <info_fotografia@icloud.com>
2024-09-05 21:18:38 +0200

    Creado el State para las Actividades

--- src/componentes/Form.tsx
+++ src/componentes/Form.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react"
 import { categories } from "../data/categories"
 
 
 export default function Form() {
+
+    const [activity, setActivity] = useState({
+        category: 1,
+        name: '',
+        calories: 0
+    });
+
+
   return (
     <form className="space-y-5 bg-white p-10 rounded-lg"
     >
@@ -10,6 +19,7 @@ export default function Form() {
             <select 
                 className="border border-slate-300 p-2 rounded-lg bg-white"
                 id="category"
+                value={activity.category}
             >
                 {categories.map(category => (
                     <option 
@@ -22,12 +32,13 @@ export default function Form() {
             </select>
         </div>
         <div className="grid grid-cols-1 gap-3" >
-            <label htmlFor="activity" className="font-bold">Actividad:</label>
+            <label htmlFor="name" className="font-bold">Actividad:</label>
             <input 
-                id="activity"
+                id="name"
                 type="text"
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+                value={activity.name}
             />
         </div>
         <div className="grid grid-cols-1 gap-3" >
@@ -37,6 +48,7 @@ export default function Form() {
                 type="number"
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Calorias:"
+                value={activity.calories}
             />
         </div>
 
