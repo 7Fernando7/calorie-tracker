72ad3cb3e507e15f4ff0c5dee4069e50e7da6323
7Fernando7 <fernandopaivap7@gmail.com>
2024-10-03 11:48:06 +0200

    Ejecutando las acciones con dispatch

--- src/componentes/Form.tsx
+++ src/componentes/Form.tsx
@@ -1,9 +1,15 @@
-import { useState, ChangeEvent, FormEvent } from "react"
-import { Activity } from "../types";
+import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
 import { categories } from "../data/categories"
 
+import type { Activity } from "../types";
+import { ActivityActions } from "../reducers/activity-reducer";
 
-export default function Form() {
+type FormProps = {
+    dispatch: Dispatch<ActivityActions>
+}
+
+
+export default function Form({dispatch}: FormProps) {
 
     const [activity, setActivity] = useState<Activity>({
         category: 1,
@@ -30,7 +36,8 @@ export default function Form() {
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        console.log('Submit...')
+        
+        dispatch({ type: 'save-activity', payload: {newActivity: activity}})
     }
 
   return (
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -23,4 +23,6 @@ export const activityReducer = (
         console.log('desde el type de save-activity ')
     }
 
+    return state
+
 }
