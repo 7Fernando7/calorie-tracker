e3005a7fa2055111a79f3200f901ece08d24e2bc
7Fernando7 <fernandopaivap7@gmail.com>
2024-10-03 12:06:23 +0200

    Reiniciando el Formulario despúes de añadir una actividad

--- src/componentes/Form.tsx
+++ src/componentes/Form.tsx
@@ -8,14 +8,15 @@ type FormProps = {
     dispatch: Dispatch<ActivityActions>
 }
 
+const initialState = {
+    category: 1,
+    name: '',
+    calories: 0
+}
 
 export default function Form({dispatch}: FormProps) {
 
-    const [activity, setActivity] = useState<Activity>({
-        category: 1,
-        name: '',
-        calories: 0
-    });
+    const [activity, setActivity] = useState<Activity>(initialState);
 
     
     const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
@@ -38,6 +39,8 @@ export default function Form({dispatch}: FormProps) {
         e.preventDefault()
         
         dispatch({ type: 'save-activity', payload: {newActivity: activity}})
+
+        setActivity(initialState)
     }
 
   return (
