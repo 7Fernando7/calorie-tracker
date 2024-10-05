7acc05011b5fa97e29a3bbaef1567f24e029dc69
7Fernando7 <fernandopaivap7@gmail.com>
2024-10-05 20:59:20 +0200

    Actualizando las actividades

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -21,9 +21,18 @@ export const activityReducer = (
 
     if(actions.type === 'save-activity') {
         // Este código maneja la lógica para actualizar el state
+        let updatedActivities : Activity[] = []
+        if(state.activeId) {
+            updatedActivities = state.activities.map(activity => activity.id === state.activeId? actions.payload.newActivity : activity )
+
+        } else{
+            updatedActivities = [...state.activities, actions.payload.newActivity]
+        }
+
         return {
             ...state,
-            activities: [...state.activities, actions.payload.newActivity]
+            activities: updatedActivities,
+            activeId: ''
         }
     }
 
