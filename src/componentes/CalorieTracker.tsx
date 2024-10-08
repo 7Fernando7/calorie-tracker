1b0aa7ac7455f450fecd4228fc93c138f0475ec6
7Fernando7 <fernandopaivap7@gmail.com>
2024-10-08 12:39:37 +0200

    Mostrando el diferencial de Calorias

--- src/componentes/CalorieTracker.tsx
+++ src/componentes/CalorieTracker.tsx
@@ -13,6 +13,8 @@ export default function CalorieTracker({activities} : CalorieTrackerProps) {
   const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0 ), [activities])
 
   const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0 ), [activities])
+
+  const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [caloriesBurned, caloriesConsumed]  )
   return (
     <>
     <h2 className="text-4xl font-black text-white text-center" >Resumen de Calorias</h2>
@@ -24,6 +26,11 @@ export default function CalorieTracker({activities} : CalorieTrackerProps) {
         <CaloriesDisplay
           calories={caloriesBurned}
           text="Quemadas"
+        />
+
+        <CaloriesDisplay
+          calories={netCalories}
+          text="Diferencia"
         />
       </div>
     
